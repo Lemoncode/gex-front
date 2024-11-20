fe0809a7d6e86c1457f9228689c6443f71010d3b
Manolo <manueldoradoserrano@MacBook-Pro-de-Manuel.local>
2024-11-21 00:50:10 +0100

    wip

--- src/modules/users/list/users.pod.tsx
+++ src/modules/users/list/users.pod.tsx
@@ -3,17 +3,9 @@ import { Link } from '@tanstack/react-router';
 import * as classes from './users.styles';
 
 export const UsersPod: React.FC = () => {
-  const [users, setUsers] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => setUsers(json));
-  }, []);
-
   return (
     <div className={classes.root}>
-      <h1>Soy la página de listado de usuarios {users}</h1>
+      <h1>Soy la página de listado de usuarios</h1>
       <Link to="/users/$id" params={{ id: '1' }}>
         Navegar a detalle de usuario
       </Link>
