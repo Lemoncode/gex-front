a323d81ad51d3eee89fb7cb5dcd1882b2779da74
Manolo <manueldoradoserrano@MacBook-Pro-de-Manuel.local>
2024-12-02 10:47:19 +0100

    remove table from user detail

--- src/modules/users/detail/user-detail.styles.ts
+++ src/modules/users/detail/user-detail.styles.ts
@@ -1,39 +1,8 @@
 import { css } from '@emotion/css';
-import { theme } from '#core/theme/theme.ts';
 
 export const root = css`
   display: flex;
   flex-direction: column;
   gap: 30px;
   align-items: center;
-`;
-
-export const title = css`
-  display: flex;
-  gap: 10px;
-  height: 50px;
-  align-items: center;
-  justify-content: space-around;
-  width: 100%;
-`;
-
-export const table = css`
-  min-width: 1200px;
-`;
-
-export const head = css`
-  background-color: ${theme.palette.primary.dark};
-  color: ${theme.palette.primary.contrastText};
-`;
-
-export const row = css`
-  &:nth-of-type(odd) {
-    background-color: ${theme.palette.grey[100]};
-  }
-`;
-
-export const pagination = css`
-  display: flex;
-  justify-content: center;
-  padding: 10px;
 `;
