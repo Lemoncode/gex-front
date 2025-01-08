0ddfbed9bb80d493ec7b35c6cd22b79fc40c4db1
Manolo <manueldoradoserrano@MacBook-Pro-de-Manuel.local>
2025-01-08 22:39:08 +0100

    add reactquerydevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 import { queryClient } from './core/react-query';
 import { router } from './core/router';
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <App />
+        <ReactQueryDevtools />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
