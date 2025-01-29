571a2daaf2dcb4b2862617a194b141d827e3c0ae
Alber EE <122263897+Alber-Writer@users.noreply.github.com>
2025-01-29 10:48:14 +0100

    apply fixes and simplificate email-availability-validation

--- src/modules/users/create/validations/available-email.validator.ts
+++ src/modules/users/create/validations/available-email.validator.ts
@@ -3,7 +3,6 @@ import * as api from '../api';
 
 const validatorType = 'CHECK_EMAIL_AVAILABILITY';
 let defaultMessage = 'Email no disponible, ya está asignado a otro usuario.';
-export const setErrorMessage = (message: string) => (defaultMessage = message);
 
 export const availableEmailValidation: FieldValidationFunctionAsync = async fieldValidatorArgs => {
   const { value, message = defaultMessage } = fieldValidatorArgs;
@@ -12,6 +11,6 @@ export const availableEmailValidation: FieldValidationFunctionAsync = async fiel
   return {
     type: validatorType,
     succeeded: !isAnExistingEmail,
-    message: isAnExistingEmail ? (Array.isArray(message) ? message.join(', ') : message) : '',
+    message: isAnExistingEmail ? (message as string) : '',
   };
 };
