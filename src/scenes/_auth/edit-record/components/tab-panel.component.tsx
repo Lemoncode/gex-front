2fc05ed8d165e712199cd275fccbed955e405c8b
manudous <manudous@gmail.com>
2025-02-19 14:11:24 +0100

    Refactor import statement for useWithTheme in tab-panel component

--- src/scenes/_auth/edit-record/components/tab-panel.component.tsx
+++ src/scenes/_auth/edit-record/components/tab-panel.component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useWithTheme } from '#core/theme/theme.hooks.ts';
+import { useWithTheme } from '#core/theme';
 import * as innerClasses from './tab-panel.styles';
 
 interface TabPanelProps {
