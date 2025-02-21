e5a885b9af574c659c1937e8ac84bf8f6d44505e
manudous <manudous@gmail.com>
2025-02-21 13:36:46 +0100

    Add CreateRecordPod component and integrate with RecordsPod for new record creation

---
+++ src/modules/records/create-record/create-record.styles.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react';
+
+export const root = () => css`
+  width: 900px;
+  height: 100%;
+`;
--- src/modules/records/list/records.pod.tsx
+++ src/modules/records/list/records.pod.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { NavigationButton } from '#common/components';
+import { Button } from '@mui/material';
+import { CreateRecordPod } from '../create-record';
 
 export const RecordsPod: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+  const toggleModal = () => setIsOpen(!isOpen);
   return (
-    <NavigationButton
-      text="Editar expediente"
-      path="/edit-record/$id"
-      params={{
-        id: '123456',
-      }}
-    />
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <Button variant="contained" color="primary" onClick={toggleModal}>
+        Crear expediente
+      </Button>
+      <NavigationButton
+        text="Editar expediente"
+        path="/edit-record/$id"
+        params={{
+          id: '123456',
+        }}
+      />
+      <CreateRecordPod isOpen={isOpen} toggleModal={toggleModal} />
+    </div>
   );
 };
