e7d9d2663e3d92a2e6baea99657ee2e3ae95f6a8
mrossi <r.rossimarcos@gmail.com>
2025-03-14 17:18:27 +0100

    refactor: update edit financial information pod

--- src/modules/records/edit-financial-information/edit-financial-information.pod.tsx
+++ src/modules/records/edit-financial-information/edit-financial-information.pod.tsx
@@ -1,5 +1,12 @@
+import { EditFinancialInformation } from './edit-financial-information.component';
 import React from 'react';
 
 export const EditFinancialInformationPod: React.FC = () => {
-  return <h2>Datos Económicos</h2>;
+  // TODO: remove after implementation
+  const annuitieRows = [
+    { year: 2022, amount: 1234, iva: 1111, id: 1, symbol: '€' },
+    { year: 2023, amount: 5678, iva: 2222, id: 2, symbol: '€' },
+    { year: 2024, amount: 9123, iva: 3333, id: 3, symbol: '€' },
+  ];
+  return <EditFinancialInformation onSubmit={() => {}} annuitieRows={annuitieRows} />;
 };
