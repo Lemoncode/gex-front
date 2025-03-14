785d9d9a867b7e522f17800ecac4b140bdb2738a
mrossi <r.rossimarcos@gmail.com>
2025-03-14 17:16:44 +0100

    feat: add base budget information form form + styling and index update

---
+++ src/modules/records/edit-financial-information/components/index.tsx
@@ -0,0 +1,4 @@
+export * from './record-iva-form.component';
+export * from './record-adjudication-form.component';
+export * from './record-base-budget-form.component';
+export * from './record-annuities-list.component';
