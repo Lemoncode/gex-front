fe79e951c5578cb768523bec4ea5e748c4a8ec2d
manudous <manudous@gmail.com>
2025-05-14 09:15:06 +0200

    feat: implement Expedientes component and refactor ExpedientesPod for improved structure

--- src/modules/expedientes/list/expedientes.pod.tsx
+++ src/modules/expedientes/list/expedientes.pod.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
 import { Spinner } from '#common/components';
-import { useWithTheme } from '#core/theme/theme.hooks.ts';
-import { useCreateExpedienteContext } from '#modules/expedientes/core/providers';
 import { usePagination } from './use-pagination-hook';
-import { TableComponent } from './components';
-import { useColumns } from './expedientes.columns';
+import { Expedientes } from './expedientes.component';
 import { CreateExpedientePod } from '../crear';
-import * as innerClasses from './expedientes.styles';
+import * as classes from './expedientes.styles';
 
 export const ExpedientesPod: React.FC = () => {
   const { expedienteCollection, currentPage, totalPages, isLoading, onPageChange } = usePagination();
-  const { onOpen } = useCreateExpedienteContext();
-  const classes = useWithTheme(innerClasses);
-  const columns = useColumns();
 
   return (
     <div className={classes.root}>
       <Spinner isSpinnerShowing={isLoading} />
-      <div className={classes.header}>
-        <Typography variant="h4">Expedientes</Typography>
-        <Button variant="contained" color="primary" onClick={onOpen}>
-          Nuevo expediente
-        </Button>
-      </div>
-      <TableComponent
-        columns={columns}
-        expedienteCollection={expedienteCollection.data}
-        totalPages={totalPages}
+      <Expedientes
+        expedienteCollection={expedienteCollection}
         currentPage={currentPage}
+        totalPages={totalPages}
         onPageChange={onPageChange}
       />
       <CreateExpedientePod />
