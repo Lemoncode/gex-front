a6f5d813db81dd49dc9029dfcfde03072297a7f0
Guste Gaubaite <219.guste@gmail.com>
2025-06-02 09:43:42 +0200

    Update component to receive submit handler and restructure form actions

--- src/modules/expedientes/editar-certificaciones/components/crear-certificacion/crear-certificacion.component.tsx
+++ src/modules/expedientes/editar-certificaciones/components/crear-certificacion/crear-certificacion.component.tsx
@@ -21,11 +21,11 @@ import { crearCertificacionValidations } from '../validations/crear-certificacio
 interface Props {
   isOpen: boolean;
   handleClose: () => void;
-  onSubmit: (values: Certificacion) => void;
+  handleSubmit: (values: Certificacion) => void;
 }
 
 export const CrearCertificacion: React.FC<Props> = props => {
-  const { isOpen, handleClose, onSubmit } = props;
+  const { isOpen, handleClose, handleSubmit } = props;
 
   return (
     <Dialog open={isOpen} onClose={handleClose} maxWidth="md" fullWidth>
@@ -46,87 +46,89 @@ export const CrearCertificacion: React.FC<Props> = props => {
         <Formik
           initialValues={createEmptyCertificacion()}
           enableReinitialize={true}
-          onSubmit={onSubmit}
+          onSubmit={handleSubmit}
           validate={crearCertificacionValidations.validateForm}
         >
-          {({ values }) => (
-            <Form>
-              <Box sx={{ display: 'flex', gap: '20px', marginBottom: 2 }}>
-                <TextFieldForm name="id" label="ID" type="text" required />
-                <TextFieldForm name="numeroFactura" label="Número de factura" type="text" required />
-                <TextFieldForm
-                  name="fechaFactura"
-                  label="Fecha de factura"
-                  type="date"
-                  required
-                  slotProps={{
-                    inputLabel: {
-                      shrink: true,
-                    },
-                  }}
-                />
-              </Box>
-              <Box sx={{ marginBottom: 2 }}>
-                <Typography>Periodo de gasto</Typography>
-                <Box gap={4}>
-                  <Box display="flex" alignItems="center" gap={2}>
-                    <TextFieldForm
-                      name="periodoGastoInicio"
-                      label="Fecha de inicio"
-                      type="text"
-                      required
-                      slotProps={{
-                        inputLabel: {
-                          shrink: true,
-                        },
-                        htmlInput: {
-                          readOnly: true,
-                        },
-                      }}
-                    />
-                    <span> - </span>
-                    <TextFieldForm
-                      name="periodoGastoFin"
-                      label="Fecha de fin"
-                      type="text"
-                      required
-                      slotProps={{
-                        inputLabel: {
-                          shrink: true,
-                        },
-                        htmlInput: {
-                          readOnly: true,
-                        },
-                      }}
-                    />
-                  </Box>
-                  <Box display="flex" gap={1}>
-                    <DateCalendarField
-                      name="periodoGastoInicio"
-                      label="Fecha de inicio"
-                      maxDate={values.periodoGastoFin ? dayjs(values.periodoGastoFin, 'DD/MM/YYYY') : undefined}
-                    />
-                    <Divider orientation="vertical" flexItem />
-                    <DateCalendarField
-                      name="periodoGastoFin"
-                      label="Fecha de fin"
-                      minDate={values.periodoGastoInicio ? dayjs(values.periodoGastoInicio, 'DD/MM/YYYY') : undefined}
-                    />
+          {({ values }) => {
+            return (
+              <Form>
+                <Box sx={{ display: 'flex', gap: '20px', marginBottom: 2 }}>
+                  <TextFieldForm name="id" label="ID" type="text" required />
+                  <TextFieldForm name="numeroFactura" label="Número de factura" type="text" required />
+                  <TextFieldForm
+                    name="fechaFactura"
+                    label="Fecha de factura"
+                    type="date"
+                    required
+                    slotProps={{
+                      inputLabel: {
+                        shrink: true,
+                      },
+                    }}
+                  />
+                </Box>
+                <Box sx={{ marginBottom: 2 }}>
+                  <Typography>Periodo de gasto</Typography>
+                  <Box gap={4}>
+                    <Box display="flex" alignItems="center" gap={2}>
+                      <TextFieldForm
+                        name="periodoGastoInicio"
+                        label="Fecha de inicio"
+                        type="text"
+                        required
+                        slotProps={{
+                          inputLabel: {
+                            shrink: true,
+                          },
+                          htmlInput: {
+                            readOnly: true,
+                          },
+                        }}
+                      />
+                      <span> - </span>
+                      <TextFieldForm
+                        name="periodoGastoFin"
+                        label="Fecha de fin"
+                        type="text"
+                        required
+                        slotProps={{
+                          inputLabel: {
+                            shrink: true,
+                          },
+                          htmlInput: {
+                            readOnly: true,
+                          },
+                        }}
+                      />
+                    </Box>
+                    <Box display="flex" gap={1}>
+                      <DateCalendarField
+                        name="periodoGastoInicio"
+                        label="Fecha de inicio"
+                        maxDate={values.periodoGastoFin ? dayjs(values.periodoGastoFin, 'DD/MM/YYYY') : undefined}
+                      />
+                      <Divider orientation="vertical" flexItem />
+                      <DateCalendarField
+                        name="periodoGastoFin"
+                        label="Fecha de fin"
+                        minDate={values.periodoGastoInicio ? dayjs(values.periodoGastoInicio, 'DD/MM/YYYY') : undefined}
+                      />
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
-              <TextFieldForm name="certifica" label="Certifica" required />
-            </Form>
-          )}
+                <TextFieldForm name="certifica" label="Certifica" required />
+                <DialogActions sx={{ mt: 2 }}>
+                  <Button variant="text" color="primary" onClick={handleClose}>
+                    Cancelar
+                  </Button>
+                  <Button variant="contained" color="primary" type="submit">
+                    Guardar
+                  </Button>
+                </DialogActions>
+              </Form>
+            );
+          }}
         </Formik>
-        <DialogActions>
-          <Button variant="text" color="primary" onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant="contained" color="primary" type="submit">
-            Guardar
-          </Button>
-        </DialogActions>
       </DialogContent>
     </Dialog>
   );
